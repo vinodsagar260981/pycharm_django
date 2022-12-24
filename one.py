5c31da00a54ee3d291ba0da3178c8ba46f1b3430
vinodsagar260981 <vinodsagar0981@gmail.com>
2022-12-24 13:16:33 +0530

    first file

---
+++ one.py
@@ -0,0 +1,30 @@
+# def make_prety(func):
+#     def inner():
+#         print("I got decorator")
+#         func()
+#     return inner
+# @make_prety
+# def ordinary():
+#     print("Hello")
+#
+# ordinary()
+
+# num = 13
+# flag = False
+#
+# # prime numbers are greater than 1
+# if num > 1:
+#     # check for factors
+#     for i in range(4, num):
+#         if (num % i) == 0:
+#             # if factor is found, set flag to True
+#             flag = True
+#             # break out of loop
+#             break
+#
+# # check if flag is True
+# if flag:
+#     print(num, "is not a prime number")
+# else:
+#     print(num, "is a prime number")
+
